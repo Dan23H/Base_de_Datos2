3780c5a922476219e5ed0e1a26062a05f1fe8b6e
HexDan <123597766+Dan23H@users.noreply.github.com>
2023-03-09 07:06:53 +0000

    modified:   bd2/src/App.js

--- bd2/src/App.js
+++ bd2/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Registro de personas</h1>
+      <form method="POST" action="/registro">
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required />
+        <br />
+        <label for="nombre">Nombre:</label>
+        <input type="text" id="nombre" name="nombre" required />
+        <br />
+        <label for="telefono">Teléfono:</label>
+        <input type="tel" id="telefono" name="telefono" required />
+        <br />
+        <label for="fecha_nacimiento">Fecha de nacimiento:</label>
+        <input type="date" id="fecha_nacimiento" name="fecha_nacimiento" required />
+        <br />
+        <input type="submit" value="Registrar" />
+      </form>
+      <br />
+      <hr />
+      <h1>Búsqueda de personas</h1>
+      <form method="GET" action="/busqueda">
+        <label for="email_busqueda">Email:</label>
+        <input type="email" id="email_busqueda" name="email" required /><br />
+        <input type="submit" value="Buscar" />
+      </form>
     </div>
-  );
+  )
+
 }
 
-export default App;
+export default App
